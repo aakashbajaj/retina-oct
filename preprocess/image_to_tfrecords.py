cdc495147475c25a462265e1a168eccde3321b79
Aakash <aakashbajaj5311@gmail.com>
2019-04-24 15:48:29 +0530

    image to tfrecords

---
+++ preprocess/image_to_tfrecords.py
@@ -0,0 +1,149 @@
+import re
+import os
+from tfr_utils import _write_tf_records
+from tfr_utils import build_example_list_tf
+from tfr_utils import get_example_share
+from tfr_utils import split_list
+
+import multiprocessing as mp
+pool = mp.Pool(mp.cpu_count())
+
+import tensorflow as tf
+import numpy as np
+import tensorflow.gfile as tf_reader
+
+import logging
+
+from argparse import ArgumentParser
+parser = ArgumentParser()
+
+parser.add_argument("--inp-dir", required=True, dest="INP_DIR", help="Data folder for input images")
+parser.add_argument("--ann-dir", dest="ANN_DIR", help="Data folder for annotations", default="")
+parser.add_argument("--out-dir", required=True, dest="OUT_DIR", help="Destination folder for converted records")
+parser.add_argument("--label-list", required=True, dest="LABEL_LIST", help="Location of labels json file")
+parser.add_argument("--num-shards", type=int, dest="NUM_SHARDS", default=2)
+parser.add_argument("--split-flag", type=int, dest="SPLIT_FLAG", default=1)
+parser.add_argument("--train-split", type=float, dest="TRAIN_SPLIT", default=0.8)
+parser.add_argument("--seed", type=int, dest="SEED", default=123)
+parser.add_argument("--height", type=int, dest="HEIGHT", default=224)
+parser.add_argument("--width", type=int, dest="WIDTH", default=224)
+parser.add_argument("--fraction", type=float, dest="FRACTION", default=1)
+
+args = parser.parse_args()
+
+INP_DIR = args.INP_DIR
+OUT_DIR = args.OUT_DIR
+NUM_SHARDS = int(args.NUM_SHARDS)
+SPLIT_FLAG = int(args.SPLIT_FLAG)
+TRAIN_SPLIT = float(args.TRAIN_SPLIT)
+SEED = int(args.SEED)
+HEIGHT = int(args.HEIGHT)
+WIDTH = int(args.WIDTH)
+FRACTION = float(args.FRACTION)
+LABEL_LIST = args.LABEL_LIST
+
+if tf.gfile.IsDirectory(args.ANN_DIR):
+	ANN_DIR = args.ANN_DIR
+else:
+	ANN_DIR = ""
+
+BUCKET_NAME = ""
+BUCKET_PATH = ""
+
+def _fast_write_sharded_tfrs(examples, num_shards, output_dir, image_dims, is_training=True):
+	sharded_examples = split_list(examples, num_shards)
+
+	for count, shard in enumerate(sharded_examples, start=1):
+		print("Starting %s shard %d" % ('train' if is_training else 'test', count))
+		output_filename = '{0}_{1:02d}_of_{2:02d}.tfrecord'.format('train' if is_training else 'test', count, num_shards)
+		out_filepath = os.path.join(output_dir, output_filename)
+		pool.apply_async(_write_tf_records, args=(shard, out_filepath, image_dims, ANN_DIR))
+
+# MAIN
+
+if (INP_DIR).startswith("gs://"):
+	GCS_FLAG = 1
+
+	try:
+		BUCKET_NAME = re.search("gs://(.+?)/", (INP_DIR)).group(1)
+	except AttributeError:
+		logging.exception("Invalid GS bucket name")
+
+	BUCKET_PATH = "gs://" + BUCKET_NAME
+	print("Accessing Bucket: ", BUCKET_PATH)
+
+else:
+	GCS_FLAG = 0
+
+
+if GCS_FLAG:
+	print("\nReading from GCS Bucket:")
+	examples, labels = build_example_list_tf(INP_DIR, SEED)
+else:
+	print("\nReading from local directory:")
+	examples, labels = build_example_list_tf(INP_DIR, SEED)
+
+print("Total Images found: ", len(examples))
+
+tar_size = int((FRACTION)*len(examples))
+examples = examples[:tar_size]
+print("Using:", len(examples), "images.")
+
+for k in examples:
+	tmp = np.zeros(len(labels))
+	tmp[k['label']] = 1
+	k['oh_label'] = tmp
+
+if (SPLIT_FLAG):
+
+	train_examples, test_examples = get_example_share(examples, TRAIN_SPLIT)
+
+	train_dir = os.path.join((OUT_DIR), "train")
+	test_dir = os.path.join((OUT_DIR), "test")
+
+	if not (OUT_DIR).startswith("gs://"):
+		try:
+			if not os.path.exists(train_dir):
+				os.makedirs(train_dir)
+
+			if not os.path.exists(test_dir):
+				os.makedirs(test_dir)
+
+		except Exception as e:
+			print(e)
+			exit(0)
+
+	print("Creating training shards", flush=True)
+	_fast_write_sharded_tfrs(train_examples, (NUM_SHARDS), train_dir, (HEIGHT, WIDTH))
+	print("\n", flush=True)
+	print("Creating testing shards", flush=True)
+	_fast_write_sharded_tfrs(test_examples, (NUM_SHARDS), test_dir, (HEIGHT, WIDTH), False)
+	print("\n", flush=True)
+	
+else:
+
+	if not OUT_DIR.startswith("gs://"):
+		try:
+			if not os.path.exists(OUT_DIR):
+				os.makedirs(OUT_DIR)
+
+		except Exception as e:
+			print(e)
+			exit(0)
+
+	print("Creating dataset shards", flush=True)
+	_fast_write_sharded_tfrs(examples, NUM_SHARDS, OUT_DIR, (HEIGHT, WIDTH))
+	print("\n", flush=True)
+
+pool.close()
+pool.join()
+
+print("\nWriting Labels....\n")
+import json
+try:
+	with tf_reader.GFile(LABEL_LIST, 'w') as fl:
+		json.dump(labels, fl)
+except Exception as e:
+	print(str(e))
+
+print("DONE")
---
+++ preprocess/tfr_utils.py
@@ -0,0 +1,118 @@
+import tensorflow as tf
+import os
+import cv2
+import numpy as np
+import random
+from tqdm import tqdm
+
+import io
+import logging
+from xml.dom import minidom
+
+import tensorflow.gfile as tf_reader
+
+def _bytes_feature(value):
+	return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _load_image(path, height, width, ann_dir):
+	try:
+		with tf_reader.GFile(path, 'rb') as fl:
+			image_bytes = fl.read()
+			
+			image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+
+			if ann_dir != "":
+				image = _get_ann_images(path, image, ann_dir)
+			image = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+			# print(image.astype(np.float32).shape)
+			return image.astype(np.float32)
+
+	except Exception as e:
+		print("Error Processing Image: %s\n %s" % (path, str(e)))
+		return
+
+def _get_ann_images(filepath, img_arr, ann_dir):
+	image_name = (filepath.split('/')[-1]).split(".")[-2]
+	breed_folder = filepath.split('/')[-2]
+
+	ann_filepath = os.path.join(ann_dir, breed_folder, image_name)
+	ann_xml = minidom.parse(ann_filepath)
+	xmin = int(ann_xml.getElementsByTagName('xmin')[0].firstChild.nodeValue)
+	ymin = int(ann_xml.getElementsByTagName('ymin')[0].firstChild.nodeValue)
+	xmax = int(ann_xml.getElementsByTagName('xmax')[0].firstChild.nodeValue)
+	ymax = int(ann_xml.getElementsByTagName('ymax')[0].firstChild.nodeValue)
+
+	new_img_arr = img_arr[ymin:ymax, xmin:xmax, :]
+
+	return new_img_arr
+
+def build_example_list_tf(input_dir, seed):
+	examples = []
+	labels = {}
+	class_cnt = 0
+
+	for classname in tf.gfile.ListDirectory(input_dir):
+		class_dir = os.path.join(input_dir, classname)
+		
+		if tf.gfile.IsDirectory(class_dir):	
+			for filename in tf.gfile.ListDirectory(class_dir):
+				filepath = os.path.join(class_dir, filename)
+				example = {
+					'classname': classname,
+					'path': filepath,
+					'label': class_cnt
+				}
+				examples.append(example)
+
+			labels[class_cnt] = classname
+			class_cnt = class_cnt + 1
+
+	random.seed(seed)
+	random.shuffle(examples)
+	return examples, labels
+
+
+def get_example_share(examples, train_split):
+	example_len = len(examples)
+	training_len = int(example_len*train_split)
+
+	return np.split(examples, [training_len])
+
+def split_list(tar_list, wanted_parts=1):	
+	length = len(tar_list)
+	return [tar_list[i*length//wanted_parts : (i+1)*length//wanted_parts] for i in range(wanted_parts)]
+
+def _write_tf_records(examples, output_filename, image_dims, ann_dir):
+	writer = tf.python_io.TFRecordWriter(output_filename)
+	cnt = 0
+	for example in tqdm(examples):
+		try:
+			cnt += 1	
+			image = _load_image(example['path'], height=image_dims[0], width=image_dims[1], ann_dir=ann_dir)
+			if image is not None:
+				im_str = image.tostring()
+
+				g_label = example['label']
+				
+				tf_example = tf.train.Example(features = tf.train.Features(feature = {
+					'filename': tf.train.Feature(bytes_list = tf.train.BytesList(value = [example['path'].encode('utf-8')])),
+					'image': tf.train.Feature(bytes_list = tf.train.BytesList(value = [im_str])),
+					'label': tf.train.Feature(int64_list = tf.train.Int64List(value = [g_label])),
+					'classname': tf.train.Feature(bytes_list = tf.train.BytesList(value = [example['classname'].encode('utf-8')]))
+				}))
+
+				writer.write(tf_example.SerializeToString())
+		except Exception as e:
+			print(e)
+			pass
+	writer.close()
+
+
+# not being used
+def _write_sharded_tfrecords(examples, num_shards, output_dir, image_dims, is_training=True):
+	sharded_examples = _split_list(examples, num_shards)
+
+	for count, shard in tqdm(enumerate(sharded_examples, start=1)):
+		output_filename = '{0}_{1:02d}_of_{2:02d}.tfrecord'.format('train' if is_training else 'test', count, num_shards)
+		out_filepath = os.path.join(output_dir, output_filename)
+		_write_tf_records(shard, out_filepath, image_dims)
