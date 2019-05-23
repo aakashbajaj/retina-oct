9d3cd07213c8dcebdd068e147ba322d290a5f66a
Aakash <aakashbajaj5311@gmail.com>
2019-05-23 15:45:50 +0530

    accuracy need not multiplied by 100

--- train/kubeflow_component/train.py
+++ train/kubeflow_component/train.py
@@ -179,7 +179,7 @@ metrics = {
 	'metrics': [
 		{
 			'name': 'accuracy-score', # The name of the metric. Visualized as the column name in the runs table.
-			'numberValue':  str(res['accuracy']*100.0), # The value of the metric. Must be a numeric value.
+			'numberValue':  str(res['accuracy']), # The value of the metric. Must be a numeric value.
 			'format': "PERCENTAGE",   # The optional format of the metric. Supported values are "RAW" (displayed in raw format) and "PERCENTAGE" (displayed in percentage format).
 		},
 		{
