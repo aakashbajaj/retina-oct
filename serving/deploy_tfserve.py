75fa51b011c6dce9844f1244dd39cc213d95cf79
Aakash <aakashbajaj5311@gmail.com>
2019-05-23 18:40:05 +0530

    added debug

--- serving/deploy_tfserve.py
+++ serving/deploy_tfserve.py
@@ -41,7 +41,7 @@ def main(argv=None):
   print("contents:")
   subprocess.call(['ls', '/secret/gcp-credentials'])
   print("GOOGLE_APPLICATION_CREDENTIALS:")
-  print("environment: ", os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
+  print("environment 1: ", os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
   print("user-gcp:")
   subprocess.call(['cat', '/secret/gcp-credentials/user-gcp-sa.json'])
   print("admin-gcp:")
@@ -89,6 +89,7 @@ def main(argv=None):
   args_list = ['--%s=%s' % (k.replace('_', '-'),v)
                for k,v in six.iteritems(args_dict) if v is not None]
   logging.info('Generating tfserving template.')
+  logging.info("environment 2: ", os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
 
 
   template_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tfserve-template.yaml')
