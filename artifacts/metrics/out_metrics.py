f78dca780cea8e00c2e422ae0b67bce7cd8e6902
Aakash <aakashbajaj5311@gmail.com>
2019-05-24 18:00:08 +0530

    debug metrics

---
+++ artifacts/metrics/out_metrics.py
@@ -0,0 +1,23 @@
+import json
+
+metrics = {
+	'metrics': [
+		{
+			'name': 'accuracy-score', # The name of the metric. Visualized as the column name in the runs table.
+			'numberValue':  str(0.859667), # The value of the metric. Must be a numeric value.
+			'format': "PERCENTAGE",   # The optional format of the metric. Supported values are "RAW" (displayed in raw format) and "PERCENTAGE" (displayed in percentage format).
+		},
+		{
+			'name': 'train-loss',
+			'numberValue':  str(0.321564),
+			'format': "RAW", 
+		},
+	]
+}
+
+with file_io.FileIO('/mlpipeline-metrics.json', 'w') as fl:
+	json.dump(metrics, fl)
+
+with file_io.FileIO('/mlpipeline-metrics.json', 'r') as fl:
+	read_metrics = json.load(fl)
+	print(read_metrics)
